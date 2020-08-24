c30ee4899b44d084f630af3a65560c1061e48012
ByungYeon Jang <kisagge@naver.com>
2020-08-24 14:23:29 +0900

    음료수 얼려 먹기

---
+++ liquidIcing.py
@@ -0,0 +1,41 @@
+TC = int(input())   
+ans = []
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x,y):
+        # 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if x <= -1 or x >= n or y <= -1 or y >= m:
+            return False
+        # 현재 노드를 아직 방문하지 않았다면
+        if graph[x][y] == 0:
+            # 해당 노드 방문 처리
+            graph[x][y] = 1
+            # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
+            dfs(x - 1, y)
+            dfs(x, y - 1)
+            dfs(x + 1, y)
+            dfs(x, y + 1)
+            return True
+        return False
+
+for tc in range(1, TC + 1):
+    k = "#"+str(tc) + " "
+    # N, M을 공백으로 구분하여 입력받기
+    n, m = map(int, input().split())
+
+    # 2차원 리스트의 맵 정보 입력받기
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input())))
+    
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            # 현재 위치에서 DFS 수행
+            if dfs(i, j) == True:
+                result += 1
+    k += str(result)
+    ans.append(k)
+    
+for e in ans:
+    print(e)
